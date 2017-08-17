080a0dab71a49f94fca59c74b268dd912c9ab0d6
Martin Hecher <martin@redsift.io>
2017-08-17 12:48:23 +0100

    - start webpack from gulp - remove redsift-bundler

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,14 +1,13 @@
 'use strict';
 
-var gulp = require('gulp');
-var RSBundler = require('@redsift/redsift-bundler');
+const gulp = require('gulp');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
 
-// The definitions for this Sift's bundles
-var bundles = require('./bundle.config.js');
-
-gulp.task('bundle-js', RSBundler.loadTask(gulp, 'bundle-js', bundles('js')));
-gulp.task('bundle-css', RSBundler.loadTask(gulp, 'bundle-css', bundles('css')));
-
-gulp.task('default', ['bundle-js', 'bundle-css'], function() {
-  console.log('Bundling complete');
+gulp.task('default', function(callback) {
+  webpack(webpackConfig, function (err, stats) {
+    if (err) throw err;
+    console.log(stats.toString());
+    callback();
+  });
 });
