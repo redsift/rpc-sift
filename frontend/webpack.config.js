8989a23b05de9be147afa9755b19e52a367cbac0
Martin Hecher <martin@redsift.io>
2017-08-22 22:34:27 +0100

    ported build step to webpack 3

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,6 +6,8 @@ var webpack = require('webpack'),
 var siftRootPath = path.resolve('../');
 
 module.exports = {
+  cache: true,
+  devtool: 'source-map',
   entry: {
 		controller: [ 'babel-polyfill', './src/scripts/controller.js'],
 		view: [ 'babel-polyfill', './src/scripts/view.js'],
@@ -14,33 +16,35 @@ module.exports = {
 		filename: '[name].umd-es2015.min.js',
 		path: path.join(__dirname, 'public', 'dist', 'js'),
 	},
-  cache: true,
   resolve: {
-   extensions: [ '.js', '.jsx' ]
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            require.resolve('babel-preset-es2015'),
-            require.resolve('babel-preset-es2017'),
-          ]
-        }
-      },
-    ],
+    // modules: ['node_modules'],
+    extensions: ['.js', '.jsx']
   },
   plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
   ],
-  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader?cacheDirectory',
+          options: {
+            presets: [
+              require.resolve('babel-preset-es2015'),
+              require.resolve('babel-preset-es2017'),
+            ]
+          }
+        }
+      },
+    ],
+  },
   node: {
     console: true,
     fs: 'empty',
